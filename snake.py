9e87219927520fdd4e9b78512744d96820976786
Armin Pech <a.pech@babiel.com>
2023-01-21 12:07:22 +0100

    Lass die Snake Pixel verdauen

--- snake.py
+++ snake.py
@@ -3,10 +3,10 @@
 
 # TODO
 # * Ausbruch der Snake in Ecken verhindern
-# * Farben ausscheiden (Array analog zu snake[])
-# * im Terminal anzeigen wo die Snake gerade ist (z.B. per ncurses)
+# * Snake soll nur geradeaus, rechts oder links gehen können
 # * Threading / async pixel fetch
 # * Performance-Optimierungen
+# * Rahmen definieren in dem sich die Snake bewegen darf
 
 # Protokollformat: PX <X> <Y> <color>
 
@@ -47,18 +47,25 @@ d = 1
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(p)
 
+# Erzeuge Datensatz für einen Block/Body-Teil der Snake oder auszuscheidenden Pixel
+# x und y bezeichnen die Mitte eines Blocks
+def constructblock(x, y, c):
+    data = ""
+    for xi in range(0, j):
+            for yi in range (0, j):
+                data += "PX %i %i %s\n" % (x+xi, y+yi, c)
+    return data
+
+# Sende Snake an Pixel-Server
 def sendsnake():
     data = ""
     for e in snake:
-        #data += "PX %i %i %s\nPX %i %i %s\nPX %i %i %s\n" % (e[0], e[1]-1, c, e[0], e[1], c, e[0], e[1]+1, c)
-        #data += "PX %i %i %s\n" % (e[0], e[1], c)
-        bx = e[0]-int(j/2)
-        by = e[1]-int(j/2)
-        for bxi in range(0, j):
-            for byi in range (0, j):
-                data += "PX %i %i %s\n" % (bx+bxi, by+byi, c)
+        data += constructblock(e[0]-int(j/2), e[1]-int(j/2))
     s.sendall(bytes(data, 'ascii'))
 
+# Liefe den nächsten Pixel auf den sich die Snake bewegt,
+# basierend aus der aktuellen Bewegungsrichtung und
+# unter Berücksichtigung des Pixelflut-Rahmens.
 def nextpixel(d, x, y):
     if d == 0:
         if y-j < 0:
@@ -81,29 +88,34 @@ def nextpixel(d, x, y):
             return nextpixel(2, x, y)
         return (x-j, y)
 
-i = 0
+i=0
 while True:
     i=i+1
+    # Sende die Snake an den Pixelflut-Server
     sendsnake()
-    #sleep(0.01)
+    # Bewege die Snake regelmäßig weiter
     if i > 1000:
         i=0
-        # was ist die Farbe des nächsten Pixels?
+        # Was ist der Inhalt des nächsten Pixels?
         s.send(bytes("PX %i %i\n" % nextpixel(d, snake[-1][0], snake[-1][1]), 'ascii'))
         np = s.recv(64).split()
         nx = int(np[1])
         ny = int(np[2])
-        print("next pixel: %i %i" % (nx, ny))
-        # n = next color
-        n = int("0x%s" % np[3].decode('ascii'), 16)
-        # Berechne die nächste RichtungPX %i %i %s\nPX %i %i %s\n
-        d = n % 4
-        #print("next PX %i %i %s\n" % (snake[0][0], snake[0][1], '000000'))
-        # Setze letzten Snake-Tail zurück
-        #s.sendall(bytes("PX %i %i %s\n" % (snake[0][0], snake[0][1], '000000'), 'ascii'))
-        jj=0
-        while jj < j:
-            jj=jj+1
-            snake.pop(0)
-            snake.append([nx, ny])
-s.close()
+        nc = np[3].decode('ascii')
+        print("next pixel: %i %i %s" % (nx, ny, nc))
+        # Berechne die nächste Richtung in welche sich die Snake bewegen soll,
+        # basierend auf der Farbe des Pixels auf den sich die Snake nun bewegt.
+        # Modulo 4, da es 4 Richtungen geben kann, in die sich die Snake bewegen kann.
+        d = int("0x%s" % nc, 16) % 4
+
+        # Bewege die Snake einen Block vor, indem Position des neuen Pixels ans
+        # Ende des Arrays (Kopf der Snake) angefügt wird und das erste Element entfernt wird.
+        snake.pop(0)
+        snake.append([nx, ny])
+
+        # Scheide ältesten gegessenen Pixel als Block aus
+        s.sendall(bytes(constructblock(snake[0][0], snake[0][1], dp[0]), 'ascii'))
+        dp.pop(0)
+        # Füge nach oben erfolgter Bewegung der Snake den dort zuvor zu vorhandenen
+        # Pixel zu den gegessenen Pixeln hinzu, damit er später ausgeschieden werden kann.
+        dp.append(nc)
